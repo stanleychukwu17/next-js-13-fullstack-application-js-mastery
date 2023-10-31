e0a0bd735e6b399c9501b92ac71eed59713a5754
Chukwu Stanley <stanleychukwu17@yahoo.com>
2023-10-31 11:48:28 +0100

    completed the header design

---
+++ app/components/Header/HeaderComp.tsx
@@ -0,0 +1,11 @@
+
+export default function HeaderComp() {
+    return (
+        <div className="flex justify-between items-center p-5">
+            <div className="font-bold text-lg">Prompting</div>
+            <div className="">
+                <button className="bg-black text-white p-3 rounded-full w-32">Sign in</button>
+            </div>
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 export default function HomePage() {
     return (
         <main>
-            <div className="">Hello world</div>
+
         </main>
     )
 }
