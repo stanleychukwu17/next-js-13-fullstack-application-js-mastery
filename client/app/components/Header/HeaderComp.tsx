c4df025df20c12c536239840e8a374deb32801d4
Chukwu Stanley <stanleychukwu17@yahoo.com>
2023-11-17 12:50:54 +0100

    started working on the redux provider

--- client/app/components/Header/HeaderComp.tsx
+++ client/app/components/Header/HeaderComp.tsx
@@ -4,7 +4,6 @@ import Image from "next/image"
 export default function HeaderComp() {
     const isLoggedIn = true
 
-
     return (
         <div className="flex justify-between items-center p-5">
             <div className="font-bold text-lg">
@@ -15,20 +14,17 @@ export default function HeaderComp() {
                     isLoggedIn && (
                         <div className="flex space-x-5 items-center">
                             <div className="">
-                                <button
-                                    className="bg-black text-white p-3 px-6 rounded-full"
-                                >Create new post</button>
+                                <button className="bg-black text-white p-3 px-6 rounded-full">Create new post</button>
                             </div>
                             <div className="">
-                                <button
-                                    className="bg-white text-black p-3 px-6 rounded-full border border-black"
-                                >Sign out</button>
+                                <button className="bg-white text-black p-3 px-6 rounded-full border border-black">Sign out</button>
                             </div>
                             <div className="">
                                 <Link href="/profile">
                                     <Image
-                                        src="/assets/images/logo.svg" alt="profile image"
-                                        className="" width={30} height={30}
+                                        src="/assets/images/profile.png" alt="profile image"
+                                        className="rounded-full"
+                                        width={50} height={50}
                                     />
                                 </Link>
                             </div>
--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,28 +1,30 @@
 import type { Metadata } from 'next'
+import ReduxProvider from './redux/provider'
+// import HeaderComp from '@components/Header/HeaderComp'
+import HeaderComp from './components/Header/HeaderComp'
+
 import { Inter, Source_Sans_3 } from 'next/font/google'
-import HeaderComp from '@components/Header/HeaderComp'
 import './globals.css'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Prompting',
-  description: 'Generate the next winner content using Ai',
+    title: 'Prompting',
+    description: 'Generate the next winner content using Ai',
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        <HeaderComp />
-        <div className="mainChild">
-          {children}
-        </div>
-      </body>
-    </html>
-  )
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <html lang="en">
+            <body className={inter.className}>
+                <ReduxProvider>
+                    <HeaderComp />
+                    <div className="mainChild">
+                        {children}
+                    </div>
+                </ReduxProvider>
+            </body>
+        </html>
+    )
 }
